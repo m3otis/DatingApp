c816b9fdd15c1c7303fd7dd95ff593b3d3cfcfad
Mitchell Sitohang <m.sitohang@mmguide.nl>
2018-10-22 17:00:37 +0200

    changes

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -58,6 +58,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("Computer says no!");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
@@ -87,9 +89,10 @@ namespace DatingApp.API.Controllers
 
             var token = tokenhandler.CreateToken(tokenDescriptor);
 
-            return Ok(new {
+            return Ok(new
+            {
                 token = tokenhandler.WriteToken(token)
             });
-         }
+        }
     }
 }
